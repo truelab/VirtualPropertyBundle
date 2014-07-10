d7e1a1a48ee6f35da360d4932d7a625578dc88b3
oriodesign <oriodesign@gmail.com>
2014-07-10 10:43:39 +0200

    [TRAVIS] test 3

---
+++ Tests/Generator/VirtualPropertyGeneratorBagTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Truelab\VirtualPropertyBundle\Tests\Generator;
+
+class VirtualPropertyGeneratorBagTest extends \PHPUnit_Framework_TestCase
+{
+    public function testAddGenerator()
+    {
+        // @TODO
+        $this->assertTrue(true);
+    }
+}
